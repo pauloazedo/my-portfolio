5c224abde8dfb1ad6c3232d6dcd52ba1abfc5655
pauloazedo <paulo.azedo@gmail.com>
2025-03-22 12:45:40 -0500

    fix ESLint error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section>
       <h2 className="text-3xl font-bold mb-2">Welcome</h2>
-      <p className="text-gray-700">This is my personal technical portfolio. I'm Paulo Azedo, a Senior Systems Engineer and Solutions Architect.</p>
+      <p className="text-gray-700">This is my personal technical portfolio. I&apos;m Paulo Azedo, a Senior Systems Engineer and Solutions Architect.</p>
     </section>
   );
 }
