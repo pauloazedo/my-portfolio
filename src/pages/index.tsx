c6faf4ef81f125b426d7f622a6a215f61fcc6196
pauloazedo <paulo.azedo@gmail.com>
2025-03-22 14:06:45 -0500

    Test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section>
       <h2 className="text-3xl font-bold mb-2">Welcome</h2>
-      <p className="text-gray-700">1.0 This is my personal technical portfolio. I&apos;m Paulo Azedo, a Senior Systems Engineer and Solutions Architect.</p>
+      <p className="text-gray-700">2.0 This is my personal technical portfolio. I&apos;m Paulo Azedo, a Senior Systems Engineer and Solutions Architect.</p>
     </section>
   );
 }
